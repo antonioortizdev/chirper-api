1e8748f4aad759efa222ccd6b7b41c56bbc2037f
Antonio Ortiz <antonioortizdev@gmail.com>
2022-10-31 11:11:59 +0100

    add author id to chirp

--- src/chirp/domain/Chirp.ts
+++ src/chirp/domain/Chirp.ts
@@ -1,16 +1,19 @@
 import { ChirpId } from "./value-object/ChirpId";
 import { ChirpMessage } from "./value-object/ChirpMessage";
 import { Entity } from "../../shared/domain/entity/interface/Entity";
+import { UserId } from "../../user/domain/value-object/UserId";
 
 export class Chirp implements Entity {
   constructor(
     readonly id: ChirpId,
+    readonly author: UserId,
     readonly message: ChirpMessage,
   ) {}
 
   toPrimitives(): object {
     return {
       id: String(this.id),
+      author: String(this.author),
       message: String(this.message)
     }
   }
--- test/unit/chirp/domain/Chirp.spec.ts
+++ test/unit/chirp/domain/Chirp.spec.ts
@@ -1,19 +1,24 @@
 import { Chirp } from "../../../../src/chirp/domain/Chirp"
 import { ChirpId } from "../../../../src/chirp/domain/value-object/ChirpId"
 import { ChirpMessage } from "../../../../src/chirp/domain/value-object/ChirpMessage"
+import { User } from "../../../../src/user/domain/User"
+import { UserEmail } from "../../../../src/user/domain/value-object/UserEmail"
+import { UserId } from "../../../../src/user/domain/value-object/UserId"
 
 describe('Chirp', () => {
   let chirp: Chirp
   beforeEach(() => {
     chirp = new Chirp(
       new ChirpId('c6e79858-a309-4ea6-9371-e6ccc4497fff'),
-      new ChirpMessage('This is my first chirp.')
+      new UserId('4ca63255-b896-44ec-baae-7f644a835211'),
+      new ChirpMessage('This is my first chirp.'),
     )
   })
   it('should convert chirp to primitives', () => {
     const chirpData = {
       id: 'c6e79858-a309-4ea6-9371-e6ccc4497fff',
-      message: 'This is my first chirp.'
+      author: '4ca63255-b896-44ec-baae-7f644a835211',
+      message: 'This is my first chirp.',
     }
     expect(chirp.toPrimitives()).toStrictEqual(chirpData)
   })
