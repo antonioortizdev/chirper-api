4d17dfdd455c8bc851c69295a45aebef8907445b
Antonio Ortiz <antonioortizdev@gmail.com>
2022-11-01 16:22:12 +0100

    add typeorm connection

--- src/app/infrastructure/nest/ioc/app.module.ts
+++ src/app/infrastructure/nest/ioc/app.module.ts
@@ -1,7 +1,24 @@
+import { ChirpEntity } from '../../../../chirp/infrastructure/typeorm/entity/ChirpEntity';
 import { ChirpModule } from '../../../../chirp/infrastructure/ioc/ChirpModule';
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config as dotenvConfig } from 'dotenv'
+
+dotenvConfig()
+
+const typeOrmModule = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: Number(process.env.MYSQL_PORT) || 3306,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASS || 'root',
+  database: process.env.MYSQL_DATABASE || 'chirper',
+  entities: [ChirpEntity],
+  synchronize: process.env.APP_ENV !== 'production',
+})
 
 @Module({
-  imports: [ChirpModule],
+  imports: [ChirpModule, typeOrmModule],
 })
+
 export class AppModule {}
