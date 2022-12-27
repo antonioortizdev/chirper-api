e47f58a8968846eff0e8c823551a7ca5321f21e1
Antonio Ortiz <antonioortizdev@gmail.com>
2022-12-27 19:52:00 +0100

    make chirp entity fields editable

--- src/chirp/domain/Chirp.ts
+++ src/chirp/domain/Chirp.ts
@@ -3,18 +3,32 @@ import { ChirpMessage } from "./value-object/ChirpMessage";
 import { Entity } from "../../shared/domain/entity/interface/Entity";
 import { UserId } from "../../user/domain/value-object/UserId";
 
+export interface ChirpData {
+  id: string,
+  author: string,
+  message: string,
+}
+
 export class Chirp implements Entity {
   constructor(
-    readonly id: ChirpId,
-    readonly author: UserId,
-    readonly message: ChirpMessage,
+    public id: ChirpId,
+    public author: UserId,
+    public message: ChirpMessage,
   ) {}
 
-  toPrimitives(): object {
+  toPrimitives(): ChirpData {
     return {
       id: String(this.id),
       author: String(this.author),
       message: String(this.message)
     }
+  }
+
+  static fromPrimitives({ id, author, message }: ChirpData): Chirp {
+    return new Chirp(
+      new ChirpId(id),
+      new UserId(author),
+      new ChirpMessage(message),
+    )
   }
 }
